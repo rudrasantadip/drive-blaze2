6ed8526f6873d16989c4b4d2a7dfd0d095dc4268
rudrasantadip <santadip9@gmail.com>
2024-08-24 23:29:39 +0530

    Added Devfolio Button

--- src/app/hack-synthesis/hack-synthesis.component.ts
+++ src/app/hack-synthesis/hack-synthesis.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-hack-synthesis',
   templateUrl: './hack-synthesis.component.html',
   styleUrls: ['./hack-synthesis.component.css']
 })
-export class HackSynthesisComponent {
+export class HackSynthesisComponent implements OnInit,OnDestroy{
+
+
+  // script variable
+  private script:HTMLScriptElement;
+  constructor(private renderer:Renderer2){}
+  
+
+  ngOnInit(): void {
+    this.appendScript();
+  }
+
+  ngOnDestroy(): void {
+    if(this.script)
+    {
+      this.renderer.removeChild(document.body,this.script);
+    }
+  }
+
   register() {
     window.location.href = 'https://devfolio.co';
   }
+
+  // DevFolio Button integration
+  // <script defer async src="https://apply.devfolio.co/v2/sdk.js"></script>
+  appendScript(){
+    this.script = this.renderer.createElement('script');
+    this.script.src='https://apply.devfolio.co/v2/sdk.js';
+    this.script.async=true;
+    this.script.defer=true;
+    this.renderer.appendChild(document.body,this.script);
+  }
+
 }
